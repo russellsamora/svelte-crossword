ff7adfa2b1724a34f325dff68cf15691c3371fe6
russellgoldenberg <russellgoldenberg@gmail.com>
2020-10-22 14:17:47 -0400

    fix ordering

--- src/helpers/addClueNumber.js
+++ src/helpers/addClueNumber.js
@@ -1,22 +1,18 @@
 export default function addClueNumber(data) {
   // add x/y end coords
-  const withEnds = data.map((d) => {
-    const len = d.answer.length;
-    const endX = d.x + (d.direction === "across" ? len : 0);
-    const endY = d.y + (d.direction === "down" ? len : 0);
-    const id = `${d.x}${d.y}`;
-    return { ...d, id, endX, endY };
-  });
+  const withId = data.map((d) => ({
+		...d,
+    id: `${d.x}-${d.y}`,
+  }));
 
   // sort asc by start position of clue so we have proper clue ordering
-  const maxX = Math.max(...withEnds.map((d) => d.endX));
-  data.sort((a, b) => a.y * maxX + a.x - (b.y * maxX + b.x));
+  withId.sort((a, b) => a.y - b.y || a.x - b.x);
 
   // create a lookup to store clue number (and reuse if same start pos)
   let lookup = {};
   let currentNumber = 1;
 
-  const withNumber = withEnds.map((d) => {
+  const withNumber = withId.map((d) => {
     let number;
     if (lookup[d.id]) number = lookup[d.id];
     else {
